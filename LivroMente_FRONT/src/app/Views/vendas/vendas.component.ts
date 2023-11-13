ba8d2873a20b1bf5e835daa8652e89bfd01cfb75
unknown <grappeincvinum@gmail.com>
2023-11-13 10:34:54 -0300

    Tela Carrinho

---
+++ LivroMente_FRONT/src/app/Views/vendas/vendas.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-vendas',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './vendas.component.html',
+  styleUrl: './vendas.component.css'
+})
+export class VendasComponent {
+
+}
---
+++ LivroMente_FRONT/src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CarrinhoComponent } from './Views/carrinho/carrinho.component';
+import { VendasComponent } from './Views/vendas/vendas.component';
+import { HomeComponent } from './Views/home/home.component';
+
+const routes: Routes = [
+  {path:' ', component: HomeComponent},
+  {path:'carrinho', component: CarrinhoComponent},
+  {path:'vendas', component: VendasComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
