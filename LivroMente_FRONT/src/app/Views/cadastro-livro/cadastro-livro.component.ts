17bb0b27a0750fdd26e59212cb0e9c4f5def8c9f
Time Beta <Timebeta15@gmai.com>
2023-11-27 18:34:04 -0300

    Tela home e Upload

---
+++ LivroMente_FRONT/src/app/Views/cadastro-livro/cadastro-livro.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-cadastro-livro',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './cadastro-livro.component.html',
+  styleUrl: './cadastro-livro.component.css'
+})
+export class CadastroLivroComponent {
+
+}
