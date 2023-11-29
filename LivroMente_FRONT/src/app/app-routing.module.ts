a163cd6197bb5e54161b6cee23a5a7666df4f517
Aronnnny <aron.cardoso1@gmail.com>
2023-11-29 02:16:20 -0300

    Home 1.0.4

--- LivroMente_FRONT/src/app/app-routing.module.ts
+++ LivroMente_FRONT/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { CarrinhoComponent } from './Views/carrinho/carrinho.component';
 import { VendasComponent } from './Views/vendas/vendas.component';
 import { HomeComponent } from './Views/home/home.component';
+import { DetailComponent } from './Views/detail/detail.component';
 
 const routes: Routes = [
   {path:'home', component: HomeComponent},
   {path:'carrinho', component: CarrinhoComponent},
   {path:'vendas', component: VendasComponent},
+  {path:'detalhe', component: DetailComponent},
 ];
 
 @NgModule({
