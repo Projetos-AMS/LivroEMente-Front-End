ab9da691435a8d0080634f52b2dcb28189df171f
Aronnnny <aron.cardoso1@gmail.com>
2023-12-02 00:45:45 -0300

    1.0.1

--- LivroMente_FRONT/src/app/Views/detail/detail.component.ts
+++ LivroMente_FRONT/src/app/Views/detail/detail.component.ts
@@ -1,14 +1,44 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HeaderComponent } from '../header/header.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { HeaderComponent } from '../components/header/header.component';
 
 @Component({
   selector: 'app-detail',
   standalone: true,
-  imports: [CommonModule, HeaderComponent],
+  imports: [CommonModule, HeaderComponent, FontAwesomeModule],
   templateUrl: './detail.component.html',
   styleUrl: './detail.component.css'
 })
 export class DetailComponent {
-
+  products = [
+    {
+      name: 'Livro Rosa',
+      price: '10'
+    },
+    {
+      name: 'teste2'
+    },
+    {
+      name: 'teste3'
+    },
+    {
+      name: 'teste4'
+    },
+    {
+      name: 'teste5'
+    },
+    {
+      name: 'teste6'
+    },
+    {
+      name: 'teste7'
+    },
+    {
+      name: 'teste8'
+    },
+    {
+      name: 'teste9'
+    }
+    ]
 }
--- LivroMente_FRONT/src/app/Views/home/home.component.ts
+++ LivroMente_FRONT/src/app/Views/home/home.component.ts
@@ -1,48 +1,16 @@
 import { Component, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HeaderComponent } from '../header/header.component';
+import { HeaderComponent } from '../components/header/header.component';
+import { FooterComponent } from '../components/footer/footer.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, HeaderComponent],
+  imports: [CommonModule, HeaderComponent, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
 
 export class HomeComponent {
-
-
-  
-  products = [
-    {
-      name: 'Livro Rosa',
-      price: '10'
-    },
-    {
-      name: 'teste2'
-    },
-    {
-      name: 'teste3'
-    },
-    {
-      name: 'teste4'
-    },
-    {
-      name: 'teste5'
-    },
-    {
-      name: 'teste6'
-    },
-    {
-      name: 'teste7'
-    },
-    {
-      name: 'teste8'
-    },
-    {
-      name: 'teste9'
-    }
-    ]
 
 }
