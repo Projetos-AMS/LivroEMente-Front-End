d89e7aee525ca4d0ee3ff08fe66a6f631c1e57e6
YasminBorges <yasmindasilvaborges12@gmail.com>
2024-09-24 23:56:54 -0300

    Ability to view total Orders

--- LivroMente_FRONT/src/app/views/painel-adm/painel-adm.component.ts
+++ LivroMente_FRONT/src/app/views/painel-adm/painel-adm.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatCardModule } from '@angular/material/card';
+import { MatChipsModule } from '@angular/material/chips';
+import { OrderService } from 'src/app/services/orderService/order-service.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-painel-adm',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatChipsModule, MatProgressBarModule,CommonModule,],
   templateUrl: './painel-adm.component.html',
   styleUrl: './painel-adm.component.css'
 })
-export class PainelAdmComponent {
+export class PainelAdmComponent implements OnInit {
+
+  totalOrders: any;
+
+  constructor(private _orderService: OrderService) { }
+
+  ngOnInit(): void {
+    this._orderService.getTotalOrders().subscribe((total) => {
+      this.totalOrders = total;
+      console.log(this.totalOrders)
+    });
+  }
 
 }
