3eedfe7d95ec4b8ffbbe3269d92e27f8ef1848c3
Isabela.Gomes <isabela.gomes@programmers.com.br>
2024-11-22 01:51:03 -0300

    user log

--- LivroMente_FRONT/src/app/account/shared/auth.guard.ts
+++ LivroMente_FRONT/src/app/account/shared/auth.guard.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, CanActivateFn, Router, RouterStateSnapshot } from '@angular/router';
 import { AccountService } from 'src/app/services/accountService/account.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 
 @Injectable({
@@ -8,14 +9,32 @@ import { AccountService } from 'src/app/services/accountService/account.service'
 })
 export class AuthGuard implements CanActivate {
 
-  constructor(private router: Router,private accountService: AccountService) { }
-  canActivate( next: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean  {
-  if(this.accountService.isUserLoggedIn()){
-    return true;
-  } else {
+  constructor(
+    private router: Router,
+    private accountService: AccountService,
+    private snackBar: MatSnackBar
+  ) { }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+    const user = this.accountService.getCurrentUser();
+    console.log(user)
+
+    if (user) {
+      if(user.role && route.data['roles']?.includes(user.role)) {
+        console.log("role:" + user.role +"data: " + route.data['roles']?.includes(user.role));
+        return true;
+      }
+      this.snackBar.open('Você não tem permissão para acessar esta página.', 'Fechar', {
+        duration: 3000,
+        horizontalPosition: 'center',
+        verticalPosition: 'top',
+      });
+      this.router.navigate(['']);
+      return false;
+    }
+
     this.router.navigate(['login']);
     return false;
-  }
-}
 
-};
+  };
+}
